59c271a3e06e2c17cb66fd7d27e7a9dfee7ac832
Kacktale <kimyoungha1205@naver.com>
2024-09-24 13:32:31 +0900

    fix

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,9 +1,18 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const[count,SetCount] = useState(0);
+
+  function handleButtonClick(){
+    SetCount(prev=>prev+1);
+  }
+
   return (
     <Stack>
       <Typography>Index Page</Typography>
+      <Typography variant='h1'>{count}</Typography>
+      <Button onClick={handleButtonClick}>Count</Button>
     </Stack>
   );
 }
